749c0c8e0e19b9e213ee414eb710319182338b4a
Azizbek <azizbekyakubov2003@mail.ru>
2023-04-11 14:08:07 +0500

    finish_line

--- loaders.py
+++ loaders.py
@@ -4,12 +4,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from configs import BOT_TOKEN
-from filters.filters import TrueAdmin
 
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 bot.admins = list(map(int, os.getenv("ADMINS_ID").split(",")))
-
-
-dp.filters_factory.bind(TrueAdmin)
